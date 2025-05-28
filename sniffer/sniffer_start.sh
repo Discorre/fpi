ae9fbee4f02169aea34365e1be7396e28800ef07
Die Krummy <egorich.mih05@gmail.com>
2025-05-28 13:05:38 +0700

    add install and setuo ufw to script

--- sniffer/sniffer_start.sh
+++ sniffer/sniffer_start.sh
@@ -19,6 +19,30 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Проверяем, что ufw установлен
+if ! command -v ufw &>/dev/null; then
+  echo "ufw не установлен. Устанавливаю..."
+  sudo apt update && sudo apt install -y ufw
+fi
+
+# Настраиваем политики  по умолчанию, чтобы установка сниффера не влияла на остальные сервисы на сервере
+sudo ufw default allow incoming
+sudo ufw default allow outgoing
+
+# Разрешаем доступ на порты только из подсети VPN
+sudo ufw allow from 10.8.0.0/24 to any port 8082
+sudo ufw allow from 10.8.0.0/24 to any port 8081
+
+# Запрещаем доступ к этим портам для остальных
+sudo ufw deny 8082
+sudo ufw deny 8081
+
+# Включаем ufw, если он ещё не включён
+if ! sudo ufw status | grep -q "Status: active"; then
+  echo "Включаю ufw..."
+  sudo ufw --force enable
+fi
+
 # Запускаем mitmweb с твоими параметрами
 mitmweb --mode reverse:http://127.0.0.1:8000 \
   --web-host 0.0.0.0 --listen-port 8080 \
